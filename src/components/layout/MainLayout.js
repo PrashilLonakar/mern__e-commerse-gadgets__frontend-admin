382d0c5e1a533a56e782edbc03ad2778101af1ec
PrashilLonakar <prashil.lonakar@gmail.com>
2023-04-11 03:46:17 +0530

    header done

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,7 +1,7 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Layout, Menu, theme } from "antd";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 
 import { AiOutlineDashboard, AiOutlineBgColors } from "react-icons/ai";
 import { BiCategory, BiAddToQueue } from "react-icons/bi";
@@ -9,10 +9,12 @@ import { HiClipboardList } from "react-icons/hi";
 import { FiUsers } from "react-icons/fi";
 import { SiBrandfolder } from "react-icons/si";
 import { RiCoupon3Line } from "react-icons/ri";
+import { HiUserCircle } from "react-icons/hi";
 
 import { BsCart4, BsCardList, BsFillQuestionDiamondFill } from "react-icons/bs";
 import { FaBlogger, FaBlog } from "react-icons/fa";
 import { TbBrandAmigo, TbBrandProducthunt } from "react-icons/tb";
+import { IoIosNotifications } from "react-icons/io";
 
 const { Header, Sider, Content } = Layout;
 const MainLayout = () => {
@@ -25,7 +27,13 @@ const MainLayout = () => {
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-3 mb-0">
+            <span className="sm-logo">AJ</span>
+            <span className="lg-logo">Ama-John</span>
+          </h2>
+          Ama-john
+        </div>
         <Menu
           theme="dark"
           mode="inline"
@@ -39,83 +47,83 @@ const MainLayout = () => {
           items={[
             {
               key: "",
-              icon: <AiOutlineDashboard className="fs-4" />,
+              icon: <AiOutlineDashboard className="fs-5" />,
               label: "Dashboard",
             },
             {
               key: "customers",
-              icon: <FiUsers className="fs-4" />,
+              icon: <FiUsers className="fs-5" />,
               label: "Customers",
             },
             {
               key: "catalog",
-              icon: <BsCart4 className="fs-4" />,
+              icon: <BsCart4 className="fs-5" />,
               label: "Catalogs",
               children: [
                 {
                   key: "product",
-                  icon: <TbBrandProducthunt className="fs-4" />,
+                  icon: <TbBrandProducthunt className="fs-5" />,
                   label: "Product",
                   children: [
                     {
                       key: "product/add",
-                      icon: <BiAddToQueue className="fs-4" />,
+                      icon: <BiAddToQueue className="fs-5" />,
                       label: "Add",
                     },
                     {
                       key: "product/list",
-                      icon: <BsCardList className="fs-4" />,
+                      icon: <BsCardList className="fs-5" />,
                       label: "List",
                     },
                   ],
                 },
                 {
                   key: "brand",
-                  icon: <SiBrandfolder className="fs-4" />,
+                  icon: <SiBrandfolder className="fs-5" />,
                   label: "Brand",
                   children: [
                     {
                       key: "brand/add",
-                      icon: <BiAddToQueue className="fs-4" />,
+                      icon: <BiAddToQueue className="fs-5" />,
                       label: "Add",
                     },
                     {
                       key: "brand/list",
-                      icon: <BsCardList className="fs-4" />,
+                      icon: <BsCardList className="fs-5" />,
                       label: "List",
                     },
                   ],
                 },
                 {
                   key: "category",
-                  icon: <BiCategory className="fs-4" />,
+                  icon: <BiCategory className="fs-5" />,
                   label: "Category",
                   children: [
                     {
                       key: "category/add",
-                      icon: <BiAddToQueue className="fs-4" />,
+                      icon: <BiAddToQueue className="fs-5" />,
                       label: "Add",
                     },
                     {
                       key: "category/list",
-                      icon: <BsCardList className="fs-4" />,
+                      icon: <BsCardList className="fs-5" />,
                       label: "List",
                     },
                   ],
                 },
                 {
                   key: "color",
-                  icon: <AiOutlineBgColors className="fs-4" />,
+                  icon: <AiOutlineBgColors className="fs-5" />,
                   label: "Color",
                   children: [
                     {
                       key: "color/add",
-                      icon: <BiAddToQueue className="fs-4" />,
+                      icon: <BiAddToQueue className="fs-5" />,
                       label: "Add",
                     },
                     {
                       key: "color/list",
-                      icon: <BsCardList className="fs-4" />,
+                      icon: <BsCardList className="fs-5" />,
                       label: "List",
                     },
                   ],
@@ -124,63 +132,63 @@ const MainLayout = () => {
             },
             {
               key: "orders",
-              icon: <HiClipboardList className="fs-4" />,
+              icon: <HiClipboardList className="fs-5" />,
               label: "Orders",
             },
             {
               key: "blogs",
-              icon: <FaBlogger className="fs-4" />,
+              icon: <FaBlogger className="fs-5" />,
               label: "Blog",
               children: [
                 {
                   key: "blog/add",
-                  icon: <BiAddToQueue className="fs-4" />,
+                  icon: <BiAddToQueue className="fs-5" />,
                   label: "Add",
                 },
                 {
                   key: "blog/list",
-                  icon: <BsCardList className="fs-4" />,
+                  icon: <BsCardList className="fs-5" />,
                   label: "List",
                 },
               ],
             },
             {
               key: "blog-category",
-              icon: <FaBlog className="fs-4" />,
+              icon: <FaBlog className="fs-5" />,
               label: "Blog-Category",
               children: [
                 {
                   key: "blog-category/add",
-                  icon: <BiAddToQueue className="fs-4" />,
+                  icon: <BiAddToQueue className="fs-5" />,
                   label: "Add",
                 },
                 {
                   key: "blog-category/list",
-                  icon: <BsCardList className="fs-4" />,
+                  icon: <BsCardList className="fs-5" />,
                   label: "List",
                 },
               ],
             },
             {
               key: "coupon",
-              icon: <RiCoupon3Line className="fs-4" />,
+              icon: <RiCoupon3Line className="fs-5" />,
               label: "Coupons",
               children: [
                 {
                   key: "coupon/add",
-                  icon: <BiAddToQueue className="fs-4" />,
+                  icon: <BiAddToQueue className="fs-5" />,
                   label: "Add",
                 },
                 {
                   key: "coupon/list",
-                  icon: <BsCardList className="fs-4" />,
+                  icon: <BsCardList className="fs-5" />,
                   label: "List",
                 },
               ],
             },
             {
               key: "inquiries",
-              icon: <BsFillQuestionDiamondFill className="fs-4" />,
+              icon: <BsFillQuestionDiamondFill className="fs-5" />,
               label: "Inquiries",
             },
           ]}
@@ -188,6 +196,7 @@ const MainLayout = () => {
       </Sider>
       <Layout className="site-layout">
         <Header
+          className="d-flex justify-content-between px-3 pe-3"
           style={{
             padding: 0,
             background: colorBgContainer,
@@ -200,6 +209,23 @@ const MainLayout = () => {
               onClick: () => setCollapsed(!collapsed),
             }
           )}
+          <div className="d-flex gap-3 align-items-center">
+            <div className="position-relative">
+              <IoIosNotifications style={{ "font-size": "25px" }} />
+              <div className="badge bg-warning rounded-circle p-1 position-absolute">
+                3
+              </div>
+            </div>
+            <div className="d-flex gap-3 align-items-center">
+              <div>
+                <HiUserCircle style={{ "font-size": "60px" }} />
+              </div>
+              <div>
+                <h5 className="mb-0">Dummy</h5>
+                <p className="mb-0">dummybummy@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </Header>
         <Content
           style={{
@@ -209,7 +235,7 @@ const MainLayout = () => {
             background: colorBgContainer,
           }}
         >
-          Content
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
