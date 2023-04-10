923b3f43d978b8682c05b0adb54e0ae6bff01447
PrashilLonakar <prashil.lonakar@gmail.com>
2023-04-11 03:07:14 +0530

    menu added

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,18 +1,27 @@
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Layout, Menu, theme } from "antd";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AiOutlineDashboard, AiOutlineBgColors } from "react-icons/ai";
+import { BiCategory, BiAddToQueue } from "react-icons/bi";
+import { HiClipboardList } from "react-icons/hi";
+import { FiUsers } from "react-icons/fi";
+import { SiBrandfolder } from "react-icons/si";
+import { RiCoupon3Line } from "react-icons/ri";
+
+import { BsCart4, BsCardList, BsFillQuestionDiamondFill } from "react-icons/bs";
+import { FaBlogger, FaBlog } from "react-icons/fa";
+import { TbBrandAmigo, TbBrandProducthunt } from "react-icons/tb";
+
 const { Header, Sider, Content } = Layout;
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const navigate = useNavigate();
+
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -20,22 +29,159 @@ const MainLayout = () => {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key == "signout") {
+            } else {
+              navigate(key);
+            }
+          }}
           items={[
             {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashboard",
             },
             {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              key: "customers",
+              icon: <FiUsers className="fs-4" />,
+              label: "Customers",
             },
             {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
+              key: "catalog",
+              icon: <BsCart4 className="fs-4" />,
+              label: "Catalogs",
+              children: [
+                {
+                  key: "product",
+                  icon: <TbBrandProducthunt className="fs-4" />,
+                  label: "Product",
+                  children: [
+                    {
+                      key: "product/add",
+                      icon: <BiAddToQueue className="fs-4" />,
+                      label: "Add",
+                    },
+                    {
+                      key: "product/list",
+                      icon: <BsCardList className="fs-4" />,
+                      label: "List",
+                    },
+                  ],
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                  children: [
+                    {
+                      key: "brand/add",
+                      icon: <BiAddToQueue className="fs-4" />,
+                      label: "Add",
+                    },
+                    {
+                      key: "brand/list",
+                      icon: <BsCardList className="fs-4" />,
+                      label: "List",
+                    },
+                  ],
+                },
+                {
+                  key: "category",
+                  icon: <BiCategory className="fs-4" />,
+                  label: "Category",
+                  children: [
+                    {
+                      key: "category/add",
+                      icon: <BiAddToQueue className="fs-4" />,
+                      label: "Add",
+                    },
+                    {
+                      key: "category/list",
+                      icon: <BsCardList className="fs-4" />,
+                      label: "List",
+                    },
+                  ],
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color",
+                  children: [
+                    {
+                      key: "color/add",
+                      icon: <BiAddToQueue className="fs-4" />,
+                      label: "Add",
+                    },
+                    {
+                      key: "color/list",
+                      icon: <BsCardList className="fs-4" />,
+                      label: "List",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <HiClipboardList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "blogs",
+              icon: <FaBlogger className="fs-4" />,
+              label: "Blog",
+              children: [
+                {
+                  key: "blog/add",
+                  icon: <BiAddToQueue className="fs-4" />,
+                  label: "Add",
+                },
+                {
+                  key: "blog/list",
+                  icon: <BsCardList className="fs-4" />,
+                  label: "List",
+                },
+              ],
+            },
+            {
+              key: "blog-category",
+              icon: <FaBlog className="fs-4" />,
+              label: "Blog-Category",
+              children: [
+                {
+                  key: "blog-category/add",
+                  icon: <BiAddToQueue className="fs-4" />,
+                  label: "Add",
+                },
+                {
+                  key: "blog-category/list",
+                  icon: <BsCardList className="fs-4" />,
+                  label: "List",
+                },
+              ],
+            },
+            {
+              key: "coupon",
+              icon: <RiCoupon3Line className="fs-4" />,
+              label: "Coupons",
+              children: [
+                {
+                  key: "coupon/add",
+                  icon: <BiAddToQueue className="fs-4" />,
+                  label: "Add",
+                },
+                {
+                  key: "coupon/list",
+                  icon: <BsCardList className="fs-4" />,
+                  label: "List",
+                },
+              ],
+            },
+            {
+              key: "inquiries",
+              icon: <BsFillQuestionDiamondFill className="fs-4" />,
+              label: "Inquiries",
             },
           ]}
         />
